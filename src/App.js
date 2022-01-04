8471e4a5959bb3b4fecc72f493d0f0634cc72f6e
Ibrahim Saleh <ibrahim@gmail.com>
2022-01-04 09:16:56 -0500

    Add slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { generatePalette } from './colorHelpers';
 import Palette from './Palette';
 import seedColors from './seedColors';
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColors[2]} />
+      <Palette palette={generatePalette(seedColors[4])} />
     </div>
   );
 }
